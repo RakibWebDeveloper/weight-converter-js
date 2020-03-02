8401c2cd07c589f2a063c121866b656d92793029
RakibWebDeveloper <abdurrakib961@gmail.com>
2020-03-02 12:10:57 +0600

    Added more style

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 inputWeight = document.getElementById("lbs-input");
 
-
+document.getElementById("output").style.display = "none";
 
 inputWeight.addEventListener("input", function(e) {
+    document.getElementById("output").style.display = "block";
+
     let pounds = e.target.value;
     document.getElementById("gramsOutput").innerText = (pounds/0.0022046).toFixed(3);
     document.getElementById("kgsOutput").innerText = (pounds/2.2046).toFixed(3);
